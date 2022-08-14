0611d8fa4f1b8291fcd2323dcf7680da4ab83106
Camilo Torres <racknarow1@gmail.com>
2022-08-14 16:02:35 -0500

    first commit

---
+++ 01_HolaMundo/HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            Console.WriteLine("Mi Nombre");
+            Console.WriteLine("\n");
+            Console.WriteLine("Es Camilo");
+        }
+    }
+}
---
+++ 02_Seccion2/Seccion2/Seccion2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double altura, ancho, area, perimetro;
+
+            Console.Write("Dame la altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Dame El ancho: ");
+            ancho = Convert.ToDouble(Console.ReadLine());
+
+            area = altura * ancho;
+            perimetro = 2 * (altura + ancho);
+
+            Console.WriteLine("El area es: {0}", area);
+            Console.WriteLine("El perimetro es: {0}", perimetro);
+
+        }
+    }
+}
